4ea121026f1a1429f0f28f91c1a5942af59685d0
Anders Barfod <baffyx@gmail.com>
2016-01-19 14:59:17 +0100

    Update cvr_produktionsenhed_kibana.sql
    Ændret til nyeste version med bebyggelse, lokalplan og bebyggelsestype

--- sql/cvr_produktionsenhed_kibana.sql
+++ sql/cvr_produktionsenhed_kibana.sql
@@ -1,80 +1,32 @@
-CREATE MATERIALIZED VIEW kibana.cvr_produktionsenhed AS 
- SELECT row_number() OVER () AS gid,
-    cvr.modifikationsstatus,
-    cvr.livsforloeb_startdato::timestamp without time zone AS "Livsforløb startdato",
-    cvr.livsforloeb_ophoersdato::timestamp without time zone AS "Livsforløb ophørsdato",
-    cvr.ajourfoeringsdato::timestamp without time zone AS "Ajourføringsdato",
-    cvr.virksomhed_gyldigfra::timestamp without time zone AS "Virksomhed gyldig fra",
-    cvr.virksomhed_cvrnr AS "CVR-nummer",
-    cvr.pnr AS "P-nummer",
-    cvr.navn_tekst AS "Virksomhedsnavn",
-    cvr.navn_gyldigfra::timestamp without time zone AS "Navn gyldig fra",
-    cvr.reklamebeskyttelse,
-    cvr.beliggenhedsadresse_gyldigfra::timestamp without time zone AS "Beliggenhedsadresse gyldig fra",
-    cvr.beliggenhedsadresse_vejnavn AS "Vejnavn",
-    cvr.beliggenhedsadresse_vejkode AS "Vejkode",
-    cvr.beliggenhedsadresse_husnummerfra AS "Husnummer fra",
-    cvr.beliggenhedsadresse_husnummertil AS "Husnummer til",
-    cvr.beliggenhedsadresse_bogstavfra AS "Bogstav fra",
-    cvr.beliggenhedsadresse_bogstavtil AS "Bogstav til",
-    cvr.beliggenhedsadresse_etage AS "Etage",
-    cvr.beliggenhedsadresse_sidedoer AS "Side/dør",
-    cvr.beliggenhedsadresse_postnr AS "Postnummer",
-    cvr.beliggenhedsadresse_postdistrikt AS "Postdistrikt",
-    cvr.beliggenhedsadresse_bynavn AS "Bynavn",
-    cvr.kommune_kode AS "Kommunekode",
-    adr."Kommunenavn",
-    cvr.beliggenhedsadresse_postboks AS "Postboks",
-    cvr.beliggenhedsadresse_conavn AS "CO navn",
-    cvr.beliggenhedsadresse_adressefritekst AS "Adresse fritekst",
-    cvr.hovedbranche_gyldigfra::timestamp without time zone AS "Hovedbranche gyldig fra",
-    cvr.hovedbranche_kode AS "Hovedbranche (kode)",
-    cvr.hovedbranche_tekst AS "Hovedbranche (tekst)",
-    cvr.bibranche1_gyldigfra::timestamp without time zone AS "Bibranche 1 gyldig fra",
-    cvr.bibranche1_kode AS "Bibranche 1 (kode)",
-    cvr.bibranche1_tekst AS "Bibranche 1 (tekst)",
-    cvr.bibranche2_gyldigfra::timestamp without time zone AS "Bibranche 2 gyldig fra",
-    cvr.bibranche2_kode AS "Bibranche 2 (kode)",
-    cvr.bibranche2_tekst AS "Bibranche 2 (tekst)",
-    cvr.bibranche3_gyldigfra::timestamp without time zone AS "Bibranche 3 gyldig fra",
-    cvr.bibranche3_kode AS "Bibranche 3 (kode)",
-    cvr.bibranche3_tekst AS "Bibranche 3 (tekst)",
-    cvr.telefonnummer_gyldigfra::timestamp without time zone AS "Telefonnummer gyldig fra",
-    cvr.telefonnummer_kontaktoplysning AS "Telefonnummer",
-    cvr.email_gyldigfra::timestamp without time zone AS "E-mail gyldig fra",
-    cvr.email_kontaktoplysning AS "E-mail",
-    cvr.aarsbeskaeftigelse_aar AS "År (Årsbeskæftigelse)",
-    cvr.aarsbeskaeftigelse_antalansatte AS "Antal ansatte (Årsbeskæftigelse)",
-    cvr.aarsbeskaeftigelse_antalansatteinterval AS "Antal ansatte i interval (Årsbeskæftigelse)",
-    cvr.aarsbeskaeftigelse_antalaarsvaerk AS "Antal årsværk (Årsbeskæftigelse)",
-    cvr.aarsbeskaeftigelse_antalaarsvaerkinterval AS "Antal årsværk i interval (Årsbeskæftigelse)",
-    cvr.aarsbeskaeftigelse_antalinclejere AS "Antal incl. ejere (Årsbeskæftigelse)",
-    cvr.aarsbeskaeftigelse_antalinclejereinterval AS "Antal incl. ejere i interval (Årsbeskæftigelse)",
-    cvr.kvartalsbeskaeftigelse_aar AS "År (Kvartalsbeskaeftigelse)",
-    cvr.kvartalsbeskaeftigelse_kvartal AS "Kvartal (Kvartalsbeskaeftigelse)",
-    cvr.kvartalsbeskaeftigelse_antalansatte AS "Antal ansatte (Kvartalsbeskaeftigelse)",
-    cvr.kvartalsbeskaeftigelse_antalansatteinterval AS "Antal ansatte i interval (Kvartalsbeskaeftigelse)",
-    cvr.kvartalsbeskaeftigelse_antalaarsvaerk AS "Antal årsværk (Kvartalsbeskaeftigelse)",
-    cvr.kvartalsbeskaeftigelse_antalaarsvaerkinterval AS "Antal årsværk i interval (Kvartalsbeskaeftigelse)",
-    cvr.maanedsbeskaeftigelse_aar AS "År (Månedsbeskaeftigelse)",
-    cvr.maanedsbeskaeftigelse_maaned AS "Måned (Månedsbeskaeftigelse)",
-    cvr.maanedsbeskaeftigelse_antalansatte AS "Antal ansatte (Månedsbeskaeftigelse)",
-    cvr.maanedsbeskaeftigelse_antalansatteinterval AS "Antal ansatte i interval (Månedsbeskaeftigelse)",
-    cvr.maanedsbeskaeftigelse_antalaarsvaerk AS "Antal årsværk (Månedsbeskaeftigelse)",
-    cvr.maanedsbeskaeftigelse_antalaarsvaerkinterval AS "Antal årsværk i interval (Månedsbeskaeftigelse)",
-    cvr.hovedafdeling AS "Hovedafdeling",
-    cvr.deltagere,
-    adr."Sognnnr",
-    adr."Sognnavn",
-    adr.kn100mdk,
-    adr.kn1kmdk,
-    adr.kn10kmdk,
-    adr."Bebyggelsestype",
-    adr."Lokalplannr",
-    adr."Lokalplannavn",
-    adr."Lokalplan anvendelsespecifik",
-    adr."Lokalplan zonestatus",
-    adr."Lokalplan link",
-    adr.the_geom
-   FROM cvr.cvr_prod_enhed cvr
-     JOIN _00_grundkort.adresser_adm_rh2 adr ON concat(cvr.kommune_kode, lpad(cvr.beliggenhedsadresse_vejkode::text, 4, '0'::text), lpad(cvr.beliggenhedsadresse_husnummerfra::text, 3, '0'::text), cvr.beliggenhedsadresse_bogstavfra) = adr.adr_id::text;
+CREATE MATERIALIZED VIEW kibana.cvr_prodenhed_hrks AS
+SELECT cvr.*, plan.plannr as "LP plannummer", plan.plannavn as "LP plannavn", plan.anvendelsespecifik as "LP anvendelsespecifik", plan.anvendelsegenerel as "LP generel anvendelse", plan.zonestatus as "LP zonestatus", jord.temanavn as "Forurenet jord", doklink as "Lokalplan pdf", bebyg.feat_type as bebyggelsestype
+FROM cvr.cvr_prod_enh_hrks cvr
+LEFT JOIN --Lokalplaner
+	(SELECT plannr, plannavn, doklink, anvendelsespecifik, anvendelsegenerel, zonestatus, the_geom
+	   FROM _00_grundkort.lokalplaner_v) plan
+ON st_within(cvr.the_geom, plan.the_geom)
+LEFT JOIN --Forurenet jord
+	(SELECT temanavn, the_geom
+	   FROM _00_grundkort.dkjord_v1
+	UNION
+	 SELECT temanavn, the_geom
+	   FROM _00_grundkort.dkjord_v2) jord
+ON st_within(cvr.the_geom, jord.the_geom)
+LEFT JOIN --Bebyggelsestype fra FOT
+	(SELECT lavbebyg.the_geom,
+	    lavbebyg.feat_type
+	   FROM _00_grundkort.lavbebyg
+	UNION
+	 SELECT hojbebyg.the_geom,
+	    hojbebyg.feat_type
+	   FROM _00_grundkort.hojbebyg
+	UNION
+	 SELECT bykerne.the_geom,
+	    bykerne.feat_type
+	   FROM _00_grundkort.bykerne
+	UNION
+	 SELECT industri.the_geom,
+	    industri.feat_type
+	   FROM _00_grundkort.industri) bebyg
+ON st_within(cvr.the_geom, bebyg.the_geom)
+--LIMIT 1000
