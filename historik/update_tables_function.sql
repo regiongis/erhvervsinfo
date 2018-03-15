bfa40931e685b57e973ed9743bcb8fa547b63e2c
Jesper Svejgaard <svejgaard@gmail.com>
2018-03-15 15:23:03 +0100

    Update update_tables_function.sql

--- historik/update_tables_function.sql
+++ historik/update_tables_function.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION cvr.update_tables() RETURNS void AS $$
+CREATE OR REPLACE FUNCTION cvr.update_tables_prod() RETURNS void AS $$
 
 ----------------------------------------------------------------------------------------
 /* OPDATERER STAMTABEL */
